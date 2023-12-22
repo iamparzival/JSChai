02952fc9dba8326146054fae58cf74d70f7f576c
Yash <78196745+iamparzival@users.noreply.github.com>
2023-12-22 17:05:17 +0000

    datatypes basics completed

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let name = "Yash"
+
+let age = 37
+
+let isLoggedIn = false
+
+//TOTAL 8
+
+//number => 2 to power 53
+//bigInt
+//string => " "
+//boolean => true/false
+
+//null => standalone value (representation of nothing) //typeof(null) == object
+//undefined => value is not defined yet
+//symbol => unique
+
+//object
+
+console.log(typeof(null))
+
